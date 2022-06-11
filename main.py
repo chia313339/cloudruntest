ca16f1ab9aa6faf43f59d215ddc119384fb45826
CHIA <chia313339@gmail.com>
2022-06-11 15:57:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Web2 Hello {}!".format(name)
+    return "Web4 Hello {}!".format(name)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
